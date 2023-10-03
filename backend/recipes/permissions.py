49aa90223f2d2e354679866495d687506f94e8eb
Сергей Иванов <isv1601@yandex.ru>
2023-10-03 11:38:27 +0300

    Исправил замечание " def has_permission(self, request, view): Лишний метод."

--- backend/recipes/permissions.py
+++ backend/recipes/permissions.py
@@ -2,11 +2,6 @@ from rest_framework import permissions
 
 
 class IsAdminOrAuthorOrReadOnly(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return (
-            request.user.is_authenticated
-            or request.method in permissions.SAFE_METHODS
-        )
 
     def has_object_permission(self, request, view, obj):
         return (
